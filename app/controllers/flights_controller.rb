7884bee4f3542d1ba4830a787f949549ad3647ad
Leon Lan <leolandongfang@gmail.com>
2017-11-21 15:36:17 +0800

    fixed the database

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,15 +1,25 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.search(params[:search])
+    @flights = Flight.where(
+      origin: params[:origin],
+      destination: params[:destination],
+      departure: params[:departure],
+    )
   end
 
   def new
+    @flight = Flight.new
+    @jets = current_user.jets
   end
 
   def create
+    @flight = flight.new(flight_params)
+    @flight.save
+    redirect_to root_path
   end
 
   def show
+    @flight = Flight.find(params[:id])
   end
 
   def edit
@@ -19,5 +29,12 @@ class FlightsController < ApplicationController
   end
 
   def destroy
+    @flight = Flight.find(params[:id])
+    @flight.destroy
   end
+
+  private
+    def flight_params
+      params.require(:flight).permit(:name)
+    end
 end
