b5396a8f8289bf4d9f8c7483bc3d9177a27c222c
Leon Lan <leolandongfang@gmail.com>
2017-11-22 16:26:06 +0800

    for git hub

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121194927) do
+ActiveRecord::Schema.define(version: 20171122044556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,11 +37,11 @@ ActiveRecord::Schema.define(version: 20171121194927) do
     t.string   "destination"
     t.integer  "price"
     t.string   "status"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "capacity"
     t.datetime "arrival"
     t.datetime "departure"
-    t.integer  "capacity"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.float    "latitude"
     t.float    "longitude"
     t.index ["jet_id"], name: "index_flights_on_jet_id", using: :btree
