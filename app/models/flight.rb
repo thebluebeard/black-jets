02a6c126354ff216cb1a4466eb91a620d3e37daf
Harrison Kim <namo918@gmail.com>
2017-11-24 17:54:55 +0800

    final-push

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,7 +3,7 @@ class Flight < ApplicationRecord
   belongs_to :jet
   has_many :reviews, dependent: :destroy
   has_many :airports
-  # geocoded_by :origin
+  geocoded_by :origin
   # after_validation :geocode, if: :origin_changed?
   has_many :bookings, dependent: :destroy
 
