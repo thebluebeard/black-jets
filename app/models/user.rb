692b0fc7a780f70980232c450049f74295337b39
Benoit Mrejen <benoit.mrejen@gmail.com>
2017-11-24 15:49:40 +0800

    Fixed conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :flights
   has_many :bookings
 
+  has_many :reviews
+  after_create :send_welcome_email
+  validates :email, uniqueness: true
+
     def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
